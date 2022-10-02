02d7d82e5e6aa9a73b6f0d2269ae26d53d0aad6a
Shahar Yair <shya95@gmail.com>
2022-10-02 07:01:21 +0300

    added dockerfile and docker-compose

--- src/config.ts
+++ src/config.ts
@@ -5,7 +5,7 @@ dotenv.config({ path: path.resolve(__dirname, "../.env") });
 
 export const config = {
   googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY || "",
-  mongoDBURL: process.env.MONGOCDB_URL || "mongodb://localhost:27017/distance",
+  mongoDBURL: process.env.MONGOCDB_URL || "mongodb://mongo:27017/distance",
   baseAddress: process.env.baseAddress || "שלמה בן יוסף 1, תל אביב",
   arrival_time: process.env.arrival_time || "1667372400",
 };
